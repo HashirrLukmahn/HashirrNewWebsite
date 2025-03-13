f1027407e5537bacef58065b0259062404980702
Hashirr <hashirr_7@hotmail.com>
2025-03-12 19:58:29 -0600

    removed Contact Me button from the header for now. To be Implemented later.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -48,11 +48,11 @@ class Header extends Component {
               </a>
             </li>
 
-            <li>
+            {/*<li>
               <a className="smoothscroll" href="#contact">
                 Contact Me
               </a>
-            </li>
+            </li>*/}
           </ul>
         </nav>
         
